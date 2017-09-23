204d75ba855c029b2f1678b6751fa9a0da4c5ff1
Yawei <153379052@qq.com>
2017-09-23 06:33:26 -0400

    set up home page

---
+++ portfolio_script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Yawei
+ */
+
+// d3.selectAll(".projects_img")
+// .on("mouseover",function () {
+//     d3.select(this)
+//         .transition()
+//         .duration(100)
+//         .style("width","16rem")
+// })
+// .on("mouseout",function () {
+//     d3.selectAll(".projects_img")
+//         .transition()
+//         .duration(100)
+//         .style("width","15rem")
+// })
